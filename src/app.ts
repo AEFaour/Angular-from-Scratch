bb98516f6f154076b1a4e23bb729668200a7379e
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-06-21 11:40:12 +0200

    directive git by attribute

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,32 @@ class PhoneNumberDirective {
 
     init() {
         this.element.style.borderColor = "red";
+        this.element.addEventListener('input', (event) => {
+            const element = event.target as HTMLInputElement;
+            this.formatNumber(element);
+
+        })
+    }
+
+    formatNumber(element: HTMLInputElement) {
+
+        const value = element.value.replace(/[^\d]/g, '').substring(0, 10);
+        
+        const groups: string[] = [];
+
+        for (let i = 0; i < value.length; i += 2) {
+            groups.push(value.substring(i, i + 2))
+        }
+
+        element.value = groups.join(' ');
+        console.log(element.value);
+
     }
 }
-const element = document.querySelector<HTMLElement>('#mobile-number');
-if (element) {
+const phoneElements = document.querySelectorAll<HTMLElement>('[phone-number]');
+
+phoneElements.forEach(element => {
     const directive = new PhoneNumberDirective(element);
     directive.init();
-}
+})
+
