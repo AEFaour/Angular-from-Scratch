ff4813dd31f163feb3ba22cabc60378f4050c1ba
FAOUR Anas emmanuel <anasemmanuel.faour@infotel.com>
2023-07-10 17:04:29 +0200

    randomId

--- src/directives/user-profile.directive.ts
+++ src/directives/user-profile.directive.ts
@@ -14,9 +14,9 @@ export class UserProfileDirective {
     job: string;
 
     template = `
-    <h3>{{firstName}} {{lastName}}</h3>
+    <h3 (click)="onClickH3">{{firstName}} {{lastName}}</h3>
     <strong>Poste : </strong> {{job}}
-    <button>Changer le prénom</button>
+    <button (click)="onClickButton" (dblclick)="onDbClickButton">Changer le prénom</button>
     `;
 
     constructor(public element: HTMLElement) { }
@@ -33,11 +33,42 @@ export class UserProfileDirective {
         let renderedTemplate = this.template;
         this.template.match(/{{.*?}}/g).forEach(interpolation => {
             const propName = interpolation.replace(/{{|}}/g, '').trim();
-            
+
             renderedTemplate = renderedTemplate.replace(interpolation, this[propName]);
         });
-        
+
+        const eventsToBind: {
+            elementId: string;
+            eventName: string;
+            methodName: string
+        }[] = [];
+        this.template.match(/<.*? \(.*?\)=\".*?\".*?>/g).forEach(
+            (baliseOuverante) => {
+                const randomId="event-listener-"+ Math.ceil(Math.random() * 1000);
+                console.log(randomId);
+                baliseOuverante.match(/\(.*?\)=\".*?\"/g).forEach(
+                    (event) => {
+                        const eventName = event.match(/\(.*?\)/)[0].replace(/\(|\)/g, '');
+                        console.log(eventName)
+                        const methodName = event.match(/\".*?\"/g)[0].replace(/\"/g, '');
+                        console.log(methodName)
+                    }
+                );
+            }
+        );
+
         this.element.innerHTML = renderedTemplate;
     }
 
+    onClickButton() {
+        this.firstName = "Gloire",
+            this.render();
+    }
+    onDbClickButton() {
+        console.log("Double Click");
+    }
+
+    onClickH3() {
+        console.log("Click sur le h3");
+    }
 }
